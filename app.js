0b96071b2900b5bd91e39b9d3360b3624f2169a4
xBogdanSLL <43013230+xBogdanSLL@users.noreply.github.com>
2018-09-05 21:43:59 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+onst Discord = require('discord.js');
+const figlet = require('figlet');
+const colors = require('colors');
+const readline = require('readline');
+
+const config = require('./config.json');
+const bot = new Discord.Client();
+
+const cmdsArray = [
+    "Nu conteaza comanda.",
+    "Nu conteaza comanda.>"
+];
+
+bot.on("ready", () => {
+    clear();
+});
+
+client.login(process.env.NDg2NTk0NDE1NzM3OTYyNDk2.DnBYwA.1AaOxEyBfQpx9FdfoHyThbvpbO4);
